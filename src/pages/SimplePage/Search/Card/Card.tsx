68bfc17842c3d74949fcc4e0c14637431ef177d4
AhhYeaah <vivi26743@hotmail.com>
2023-11-05 20:26:31 -0300

    react leaflet

--- src/pages/SimplePage/Search/Card/Card.tsx
+++ src/pages/SimplePage/Search/Card/Card.tsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 
 export interface CardProps {
   props: {
-    id: number;
-    address: string;
-    name: string;
-    company: {
-      name: string;
-      id: string;
-    };
-    price: number;
+    nome: string,
+    preco: string,
+    descricao: string,
+    idQuadra:number,
+    imagemUrl: string,
+    Empresa: {
+        nome: string,
+        idEmpresa: number,
+        rua: string,
+        bairro: string,
+        numero: string,
   };
+}
 }
 
 function formatPrice(price: number) {
@@ -25,19 +29,19 @@ export function Card({ props }: CardProps) {
     <div className=" rounded-sm flex flex-row border bg-white overflow-hidden hover:shadow-xl transition-all duration-300 ">
       <div className="h-[200px] aspect-square">
         <img
-          src="https://playpiso.com.br/wp-content/uploads/2021/09/quadras-esportivas-3-playpiso.png"
+          src={props.imagemUrl}
           className="object-cover w-full h-full"
         />
       </div>
       <div className="flex p-5 flex-col justify-between items-between w-[600px]">
         <div>
           <div className="flex flex-col">
-            <span className="text-xs text-discreet font-bold">{props.address}</span>
+            <span className="text-xs text-discreet font-bold">{props.Empresa.bairro}, {props.Empresa.rua}, {props.Empresa.numero} </span>
             <span className="text-xl whitespace-nowrap overflow-hidden text-ellipsis">
-              {props.name}
+              {props.nome}
             </span>
-            <Link to={'/company/' + props.company.id}>
-              <span className="text-sm text-green-800">{props.company.name}</span>
+            <Link to={'/company/' + props.Empresa.idEmpresa}>
+              <span className="text-sm text-green-800">{props.Empresa.nome}</span>
             </Link>
           </div>
 
@@ -46,10 +50,10 @@ export function Card({ props }: CardProps) {
         <div className="flex justify-between items-center">
           <div className="flex-center flex-row  gap-1">
             <span className="text-sm font-light">R$</span>
-            <span className="bold text-xl">{formatPrice(props.price)}</span>
+            <span className="bold text-xl">{formatPrice(Number(props.preco))}</span>
           </div>
           <Link
-            to={'/allocate/' + props.id}
+            to={'/allocate/' + props.idQuadra}
             className="px-5 py-2 text-green-600 border rounded-sm border-green-500 hover:bg-green-500 hover:text-white"
           >
             Alugar
--- src/pages/SimplePage/SimplePage.tsx
+++ src/pages/SimplePage/SimplePage.tsx
@@ -7,7 +7,7 @@ interface SimplePageProp {
 
 export function SimplePage({ children: page }: SimplePageProp) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen ">
       <main className="mainPageContainer flex grow items-stretch ">{page}</main>
     </div>
   );
